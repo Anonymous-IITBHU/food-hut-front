1906ef7042c266fc9cdcab183859015240d15b0f
Anonymous-IITBHU <89541981+Anonymous-IITBHU@users.noreply.github.com>
2024-06-21 08:32:19 +0530

    Update SignUp.js

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -21,7 +21,7 @@ export default function () {
     //     location: credentials.geolocation,
     //   })
     // );
-    const response = await fetch("https://food-hut-back.vercel.app/api/createuser", {
+    const response = await fetch("https://food-hut-back.vercel.app/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
